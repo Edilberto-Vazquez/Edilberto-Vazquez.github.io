af706e6aabd136122e9d8752396fa1df2ff72ada
Edilberto <edi_vazquez12@hotmail.com>
2022-01-16 22:58:54 -0600

    added images in protafolio

--- src/utils/portafolio.js
+++ src/utils/portafolio.js
@@ -1,9 +1,14 @@
+import scientific from "../assets/images/scientific.png";
+import deavocado from "../assets/images/de-avocado-shop.png";
+import streamingVScinema from "../assets/images/streaming-vs-cinema.png";
+
 const Projects = [
   {
     projectName: "Scientific",
     information: {
       description:
         "Using the rick and morty REST API through the web site built with TypeScript and using WebComponents and displaying the first 10 characters",
+      image: scientific,
       repository: "https://github.com/Edilberto-Vazquez/scientific",
       pageOrAPI: "https://edilberto-vazquez.github.io/scientific/",
       technologiesUsed: [
@@ -23,6 +28,7 @@ const Projects = [
     information: {
       description:
         "page created to consume data from an avocado API using TypeScript and WebComponents",
+      image: deavocado,
       repository: "https://github.com/Edilberto-Vazquez/de-avocado-shop",
       pageOrAPI: "https://edilberto-vazquez.github.io/de-avocado-shop/",
       technologiesUsed: [
@@ -42,6 +48,7 @@ const Projects = [
     information: {
       description:
         "this page was created to practice with react using redux and react-router-dom",
+      image: streamingVScinema,
       repository: "https://github.com/Edilberto-Vazquez/Video-Page",
       pageOrAPI: "https://github.com/Edilberto-Vazquez/Video-Page",
       technologiesUsed: [
@@ -60,6 +67,7 @@ const Projects = [
     information: {
       description:
         "Page to comparate the price between the Cinema and Streaming platforms",
+      image: streamingVScinema,
       repository: "https://github.com/Edilberto-Vazquez/streaming-vs-cinema",
       pageOrAPI: "https://edilberto-vazquez.github.io/streaming-vs-cinema/",
       technologiesUsed: [
