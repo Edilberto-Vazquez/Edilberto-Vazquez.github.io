6ef428b129ba7422beb32583029aa3eb114deb7a
Edi <edi_vazquez12@hotmail.com>
2022-09-11 22:35:23 -0500

    chore: update header

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="color-scheme" content="light dark" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>Edilberto</title>
       </Head>
       <body>
